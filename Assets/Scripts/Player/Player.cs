02e9968098f561e48c62b9ee07242cc31977827c
JGungiah <jaimee.gungiah@gmail.com>
2025-08-05 12:47:02 +0200

    Added gravity to character controller

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,7 +3,10 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float gravity = -9.81f;
 
+    private float verticalVelocity;
+    
     private CharacterController controller;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,7 +20,27 @@ public class Player : MonoBehaviour
     {
         if (controller != null)
         {
+            if (!controller.isGrounded)
+            {
+                ApplyGravity();
+            }
+            else
+            {
+                RemoveGravity();
+            }
             movement();
+        }
+
+        
+
+        void ApplyGravity()
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+
+        void RemoveGravity()
+        {
+            verticalVelocity = -2f;
         }
 
         void movement()
@@ -26,10 +49,12 @@ public class Player : MonoBehaviour
             float verticalInput = Input.GetAxisRaw("Vertical");
             float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-            Vector3 Movement = new Vector3(horizontalInput, 0, verticalInput ).normalized;
+            Vector3 Movement = new Vector3(horizontalInput, verticalVelocity, verticalInput ).normalized;
 
             controller.Move(Movement * speed * Time.deltaTime);
 
         }
+
+       
     }
 }
