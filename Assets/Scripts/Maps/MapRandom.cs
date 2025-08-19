782d992f343381bb7dcdceef90ee530dd88f6873
gingeninge <thomassheldon539@gmail.com>
2025-08-19 11:07:15 +0200

    hotfix

--- Assets/Scripts/Maps/MapRandom.cs
+++ Assets/Scripts/Maps/MapRandom.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class MapRandom : MonoBehaviour
 {
-    public float[] maps = [0,1,2,3,4,5,6,7,8];
+    public float[] maps;
     public void mapRandomiser() 
     {
         int generatemap = Random.Range(0, maps.Length);
