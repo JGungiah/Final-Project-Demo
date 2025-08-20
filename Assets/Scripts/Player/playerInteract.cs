ab05436ebb81af94612e89e0e926d42cbeb325fc
JGungiah <jaimee.gungiah@gmail.com>
2025-08-20 17:26:34 +0200

    Interact setup

--- Assets/Scripts/Player/playerInteract.cs
+++ Assets/Scripts/Player/playerInteract.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class playerInteract : MonoBehaviour
 {
     public List<string> sceneNames;
-    
+    public GameObject interactImage;
     void Start()
     {
         
@@ -21,5 +22,29 @@ public class playerInteract : MonoBehaviour
         int RandIndex = Random.Range(0, sceneNames.Count);
         string scenetoload = sceneNames[RandIndex];
         SceneManager.LoadScene(scenetoload);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "InteractDoor")
+        {
+            interactImage.gameObject.SetActive(true);
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                interactImage.gameObject.SetActive(true);
+                SceneManager.LoadScene("LobbyRoom");
+            }
+        }
+
+       
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "InteractDoor")
+        {
+            interactImage.gameObject.SetActive(false);
+        }
     }
 }
