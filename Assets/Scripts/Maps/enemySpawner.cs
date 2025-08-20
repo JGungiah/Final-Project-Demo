284890db2f88b3264d6bfed75f7f1cd0bbbe0494
gingeninge <thomassheldon539@gmail.com>
2025-08-20 13:41:30 +0200

    wave spawning done

--- Assets/Scripts/Maps/enemySpawner.cs
+++ Assets/Scripts/Maps/enemySpawner.cs
@@ -1,16 +1,39 @@
+using System.Collections;
 using UnityEngine;
 
 public class enemySpawner : MonoBehaviour
 {
     public Transform[] SpawnLocations;
-    public GameObject[] Enemies;
-    public int count;
 
- 
-    // Update is called once per frame
+
+    public GameObject[] Enemies;
+    public int Count;
+    public GameObject[] enemyCount;
+    public GameObject gate;
+
+
+    public bool wave1;
+    public bool wave2;
+    public bool wavescomplete;
+
+    private void Start()
+    {
+        if (Count == 0 )
+        {
+            Wave1Spawn();
+        }
+
+    }
+
     void Update()
     {
-        
+       
+        countenemies();
+
+        if (wavescomplete) 
+        {
+            gate.SetActive(true);
+        }
     }
     public void Wave1Spawn() 
     {
@@ -20,16 +43,48 @@ public class enemySpawner : MonoBehaviour
         int randomIndex = Random.Range(0, Enemies.Length);
         GameObject chosenEnemy = Enemies[randomIndex];
         Instantiate(chosenEnemy, SpawnLocations[i].position, SpawnLocations[i].rotation);
-            count++;
+         
+        }
+        wave1 = true;
+    }
+
+
+    public void Wave2Spawn()
+    {
+
+        for (int i = 0; i < SpawnLocations.Length; i++)
+        {
+            int randomIndex = Random.Range(0, Enemies.Length);
+            GameObject chosenEnemy = Enemies[randomIndex];
+            Instantiate(chosenEnemy, SpawnLocations[i].position, SpawnLocations[i].rotation);
+
+        }
+       wave2 = true;
+    }
+
+
+    public void countenemies() 
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy");
+        Count = enemyCount.Length;
+
+        if(Count == 0 && wave1) 
+        {
+            wave1 = false;
+            StartCoroutine(waveinterem());
+        }
+        if (Count == 0 && wave2)
+        {
+            wave2 = false;
+            wavescomplete = true;
+           
         }
     }
-    
-    private void OnTriggerEnter(Collider other)
+
+
+    IEnumerator waveinterem() 
     {
-        if (other.gameObject.CompareTag("Player")) 
-        {
-            Wave1Spawn();
-        }
-        
+            yield return new WaitForSeconds(2f);
+            Wave2Spawn();
     }
 }
