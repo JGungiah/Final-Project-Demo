3c336b6265878b6f87c585bbcaef7a8ca6cbd8a1
JGungiah <jaimee.gungiah@gmail.com>
2025-08-26 09:34:31 +0200

    changes

--- Assets/Scripts/Camera/SingletonCamera.cs
+++ Assets/Scripts/Camera/SingletonCamera.cs
@@ -1,33 +1,33 @@
-using UnityEditor.Rendering;
-using UnityEngine;
+//using UnityEditor.Rendering;
+//using UnityEngine;
 
-public class SingletonCamera : MonoBehaviour
-{
+//public class SingletonCamera : MonoBehaviour
+//{
 
-    private static Camera _instance;
-    public static Camera Instance { get { return _instance; } }
+//    private static Camera _instance;
+//    public static Camera Instance { get { return _instance; } }
 
-    private void Awake()
-    {
-        //Singleton
-        if (_instance != null && _instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-        //else
-        //{
-        //    _instance = this.;
-        //}
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+//    private void Awake()
+//    {
+//        //Singleton
+//        if (_instance != null && _instance != this)
+//        {
+//            Destroy(this.gameObject);
+//        }
+//        //else
+//        //{
+//        //    _instance = this.;
+//        //}
+//    }
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
      
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
-}
+//    }
+//}
