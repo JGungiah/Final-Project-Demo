ae243b5a5de5a90e3c5a9597d5af520ef8c7bcb8
JGungiah <jaimee.gungiah@gmail.com>
2025-08-27 12:29:57 +0200

    Enemy changes

---
+++ Assets/Scripts/Player/Waypoints.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class Waypoints : MonoBehaviour
+{
+    //    [SerializeField] Vector3 targetPos;
+    //    private Vector3 startPos;
+    //    [SerializeField] private float waveAnimationDelay;
+    //    private bool hasStarted = false;
+    //    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //    void Start()
+    //    {
+    //        startPos = transform.position;
+    //    }
+
+    //    // Update is called once per frame
+    //    void Update()
+    //    {
+    //        if (!hasStarted)
+    //        {
+    //            StartCoroutine(WaypointAnimation());
+    //        }
+
+    //    }
+
+    //    IEnumerator WaypointAnimation()
+    //    {
+    //        hasStarted = true;
+    //        transform.position += targetPos * Time.deltaTime;
+    //        yield return new WaitForSeconds(waveAnimationDelay);
+    //        transform.position = startPos;
+    //        hasStarted = false;
+    //    }
+}
