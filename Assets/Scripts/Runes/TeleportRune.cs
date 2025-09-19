e071c6a877f9e7b2417ff8ff2487be38f164cbe1
JGungiah <jaimee.gungiah@gmail.com>
2025-09-19 16:37:32 +0200

    Rune setup

--- Assets/Scripts/Runes/TeleportRune.cs
+++ Assets/Scripts/Runes/TeleportRune.cs
@@ -5,28 +5,36 @@ public class TeleportRune : MonoBehaviour
 {
     [SerializeField] private GameObject[] runeEnter;
     [SerializeField] private GameObject[] runeExit;
-    [SerializeField] private float teleportChargeTime = 1f;
-    [SerializeField] private float teleportCooldown = 0.5f; 
+    [SerializeField] private SpriteRenderer[] enterRenderers;
+    [SerializeField] private SpriteRenderer[] exitRenderers;
+    [SerializeField] private Color[] originalColors;
+    [SerializeField] private float teleportChargeTime ;
+    [SerializeField] private float teleportCooldown ; 
 
     private GameObject player;
     private CharacterController controller;
 
     private bool isTeleporting = false;
-    private bool isOnCooldown = false;
 
+
+    private bool[] runeCooldowns;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         controller = player.GetComponent<CharacterController>();
+
+
+
+        runeCooldowns = new bool[runeEnter.Length];
     }
 
     public IEnumerator TeleportCharge(bool fromEnter, int index)
     {
-        if (isTeleporting || isOnCooldown) yield break;
+        if (isTeleporting || runeCooldowns[index]) yield break;
 
         yield return new WaitForSeconds(teleportChargeTime);
 
-        if (isTeleporting || isOnCooldown) yield break;
+        if (isTeleporting || runeCooldowns[index]) yield break;
 
         isTeleporting = true;
 
@@ -44,15 +52,33 @@ public class TeleportRune : MonoBehaviour
         controller.enabled = true;
 
 
-        StartCoroutine(TeleportCooldown());
+        StartCoroutine(TeleportCooldown(index));
 
         isTeleporting = false;
     }
 
-    private IEnumerator TeleportCooldown()
+    private IEnumerator TeleportCooldown(int index)
     {
-        isOnCooldown = true;
+        runeCooldowns[index] = true;
+
+
+        SetRuneOpacity(index, 0.05f);
+
         yield return new WaitForSeconds(teleportCooldown);
-        isOnCooldown = false;
+
+        exitRenderers[index].color = originalColors[index];
+        enterRenderers[index].color = originalColors[index];
+
+        runeCooldowns[index] = false;
+    }
+
+    private void SetRuneOpacity(int index, float alpha)
+    {
+        Color c = exitRenderers[index].color;
+        Color c2 = enterRenderers[index].color;
+        c.a = alpha;
+        c2.a = alpha;
+        enterRenderers[index].color = c2;
+        exitRenderers[index].color = c;
     }
 }
