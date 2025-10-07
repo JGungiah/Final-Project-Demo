8b41a1faad694482de4d12aa8375cc5dcfe411fb
gingeninge <thomassheldon539@gmail.com>
2025-10-07 17:36:25 +0200

    tutorial stuff

--- Assets/Scripts/Tutorial/Tutorial.cs
+++ Assets/Scripts/Tutorial/Tutorial.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
 
-    public TextMeshProUGUI WASD;
-    public TextMeshProUGUI Combo;
-    public TextMeshProUGUI Dash;
-    public TextMeshProUGUI Parry;
-    public TextMeshProUGUI Teleport;
-    public TextMeshProUGUI Enemy;
-    public TextMeshProUGUI Leave;
+    public TextMeshProUGUI tutorialText;
+   
     public int count;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,45 +15,40 @@ public class Tutorial : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-       if (count == 7) 
-        {
-            StartCoroutine(DisplayTutorial());
-            count = 0;
-        }
-    }
+  
     IEnumerator DisplayTutorial() 
     {
-       WASD.enabled = true;
-        count++;
-       yield return new WaitForSecondsRealtime(5f);
-       Combo.enabled = true;
-       WASD.enabled = false;
-        count++;    
-       yield return new WaitForSecondsRealtime(5f);
-       Combo.enabled = false;
-       Dash.enabled = true;
-        count++;
-        yield return new WaitForSecondsRealtime(5f);
-       Dash.enabled = false;
-       Parry.enabled = true;
-        count++;
-        yield return new WaitForSecondsRealtime(5f);
-        Teleport.enabled = true;
-        Parry.enabled = false;
-        count++;
-        yield return new WaitForSecondsRealtime(5f);
-        Enemy.enabled = true;
-        Teleport.enabled= false;
-        count++;
-        yield return new WaitForSecondsRealtime(5f);
-        Leave.enabled = true;
-        Enemy.enabled = false;
-        yield return new WaitForSecondsRealtime(5f);
-        Leave.enabled = false;
-        count++;
-        yield return null;
+        tutorialText.text = "Press WASD to move";
+        yield return StartCoroutine(WaitForMovementInput());
+
+        tutorialText.text = "Dash with space";
+        yield return StartCoroutine(WaitForDashInput());
+
+        tutorialText.text = "Press Right Click to Parry";
+        yield return StartCoroutine(WaitForParryInput());
+
 
     }
+    IEnumerator WaitForMovementInput()
+    {
+       
+        yield return new WaitUntil(() =>
+            Input.GetKeyDown(KeyCode.W) ||
+            Input.GetKeyDown(KeyCode.A) ||
+            Input.GetKeyDown(KeyCode.S) ||
+            Input.GetKeyDown(KeyCode.D)
+        );
+    }
+
+    IEnumerator WaitForDashInput()
+    {
+    
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+    }
+    IEnumerator WaitForParryInput()
+    {
+
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Mouse1));
+    }
 }
+
