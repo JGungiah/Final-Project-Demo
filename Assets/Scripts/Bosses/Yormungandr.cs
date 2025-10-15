15ecf5bb8ef484bfdf99a2c8328f73082e4cd3fd
JGungiah <jaimee.gungiah@gmail.com>
2025-10-15 14:01:34 +0200

    stuff

--- Assets/Scripts/Bosses/Yormungandr.cs
+++ Assets/Scripts/Bosses/Yormungandr.cs
@@ -39,6 +39,7 @@ public class Yormungandr : MonoBehaviour
     {
         if (other.gameObject.CompareTag("PlayerAttack") && !canTakeDamage && !isInvunrable)
         {
+            print(1);
             currentHealth -= playerAttack.playerDamage;
             canTakeDamage = true;
             bloodVFX.SetActive(true);
