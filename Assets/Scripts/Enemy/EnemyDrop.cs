ae3d74221d7ec25f9e5cb8d10bda43035dc45bac
JGungiah <jaimee.gungiah@gmail.com>
2025-10-23 19:29:15 +0200

    stuff

--- Assets/Scripts/Enemy/EnemyDrop.cs
+++ Assets/Scripts/Enemy/EnemyDrop.cs
@@ -1,24 +1,28 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class EnemyDrop : MonoBehaviour
 {
+    private GameObject player;
+    [SerializeField] private float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        transform.position = Vector3.MoveTowards( transform.position, player.transform.position,speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Destroy(this.gameObject, 0.1f);
+            Destroy(this.gameObject);
         }
     }
 }
